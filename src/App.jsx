5cd0e17f95a44996103fc6f6f133b67872281572
katherine <pv.katherine7@gmail.com>
2022-12-14 10:08:03 +0000

    hard coded outline of app to visualise components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.scss";
 
 const App = () => {
-  return <p>testing</p>;
+  return (
+    <body>
+      <h1>My Todos</h1>
+      <button>reset</button>
+      <input type="text" />
+      <button>append task</button>
+      <input type="checkbox" name="" id="" />
+      <p>task here</p>
+      <button>delete</button>
+    </body>
+  );
 };
 
 export default App;
---
+++ src/Components/TaskTab/TaskTab.jsx
@@ -0,0 +1,5 @@
+import "./TaskTab.scss";
+
+const TaskTab = () => {
+  return <div className="task-tab"></div>;
+};
